3649a3e9e5966bca553f9fa40e2730cdbee867cf
carl <carl.last@gmail.com>
2020-03-03 10:02:15 +0000

    Updated

--- oasysapi/oasysapi.js
+++ oasysapi/oasysapi.js
@@ -1,5 +1,17 @@
 var fs = require("fs");
 var offender = require("./offender");
 
-console.log(offender);
+console.log("DETAILS:" + offender);
+console.log("HERE");
+var  offs = [
+{"keys" : [ "offender_id", "Gender" ], "values" : [ 12345, "M" ], "response" : [ "01-01-1987", "TEST" ] } ,
+{"keys" : [ "offender_id", "Gender" ], "values" : [ 98765, "M" ], "response" : [ "01-01-1987", "TEST" ] } 
+];
 
+//{"request" : [ { "offender_id" : 98765 } ], "response" : [ {"DOB" : "02-02-1970"} ] }
+//];
+var i;
+for(i=0; i < offs[0].keys.length; i++)
+{
+console.log("Column : " + offs[0].keys[i] + " -+- Value : " + offs[0].values[i]);
+}
--- oasysapi/offender.js
+++ oasysapi/offender.js
@@ -1,5 +1,5 @@
 var fs = require("fs");
 
-var ipfile = fs.readFileSync("./offender.json","utf8");
+var ipfile = fs.readFileSync("./offender.json","utf8"); //,"utf8"
 
 module.exports = ipfile;
