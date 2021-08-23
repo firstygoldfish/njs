4ca262357082210941d8a43bd2882fd8270bfdce
Your Name <yourmail@mail.com>
2021-08-23 10:43:11 +0100

    Updated 220821

--- expressiomapi/expressiomapi.js
+++ expressiomapi/expressiomapi.js
@@ -12,39 +12,31 @@ var notfound = '{"status":"notfound"}';
 var ok = '{"status":"ok"}';
 var bad = '{"status":"badrequest"}';
 var endpoint = '';
-import {bgdata, bgdata2} from './images.js';
+import {headerbg, bgdata, bgdata2} from './images.js';
 /*-----------------------------CORE FUNCTIONS---------------------------------*/
 function badFile() {
 			console.log('ERROR:Cannot load JSON file '+jsonfile);
 			process.exit(1);
 }
 function loadJSON(filename = '') {
-	return JSON.parse(
-		fs.existsSync(filename) ? fs.readFileSync(filename).toString() : badFile()
-	);
+	return JSON.parse(fs.existsSync(filename) ? fs.readFileSync(filename).toString() : badFile() );
 }
 function saveJSON() {
 	fs.writeFile(jsonfile, JSON.stringify(offdata, null, 4), (err) => {
-	    if (err) {
-	        throw err;
-	    }
+	    if (err) { throw err; }
 	});
 }
 function securityHeader(req){
 	var secHeader = req.headers['authorization'];
-	if (secHeader !== undefined && secHeader.substring(0,7) == "Bearer "){
-		return true;
-	} else {
-		return false;
-	}
+	if (secHeader !== undefined && secHeader.substring(0,7) == "Bearer "){ return true; } else { return false; }
 }
 /*--------------------------ENDPOINT FUNCTIONS--------------------------------*/
 function listRegistrations(res) {
-	res.write('<!DOCTYPE html><html style="position: relative; min-height: 100%; background: local url(data:image/gif;base64,'+bgdata2+');">');
-	res.write('<div style="display: flex; justify-content: center"><table style="border-spacing:1px;"><tr><th style="color:#101010;padding:10px;">CRN</th>');
+	res.write(headerbg.replace('~bg~',bgdata)+'<div style="display: flex; justify-content: center">');
+	res.write('<table style="border-spacing:1px;"><tr><th style="color:#101010;padding:10px;">CRN</th>');
 	res.write('<th style="color:#101010;padding:10px;">REGISTRATIONS</th></tr>')
 	for (var i=0; i < offdata.crn.length; i++){
-		var bgcol = ((i+1)%2 === 0) ? 'B0BDDC' : 'DCCFB0'; //Odd/Even Colour
+		var bgcol = ((i+1)%2 === 0) ? 'B0BDDCC0' : 'DCCFB0C0'; //Odd/Even Colour
 		res.write('<tr><td style="background-color:#'+bgcol+';padding:5px;vertical-align:top;"><b>'+offdata.crn[i].crn+'</b>');
 		res.write('<br/><input type="button" value="Edit" onclick="javascript:window.location = \'/edit/'+offdata.crn[i].crn+'\'">');
 		res.write('</td><td style="background-color: #'+bgcol+';padding:5px">');
@@ -79,20 +71,22 @@ function edit(req, res, crn) {
 		res.write(notfound+',{crn:'+crn+'}');
 	} else {
 		var data = req.query.json; // $_GET["json"]
-		if (data != undefined) {
+		if (data !== undefined) {
 			offdata.crn[foundindex].registrations = JSON.parse(data);
 			saveJSON();
-			res.write('<!DOCTYPE html><html style="position: relative; min-height: 100%; background: local url(data:image/gif;base64,'+bgdata+');">');
-			res.write('<div style="display: flex; justify-content: center"><h2 style="color: #101010;">Changes saved.</h2></div><br/><input type="button" value="List Registrations" onclick="javascript:window.location = \'/list\'">');
-			res.write('</html>');
+			res.write(headerbg.replace('~bg~',bgdata2)+'<div style="display: flex; justify-content: center">');
+			res.write('<h2 style="color: #101010;">Changes saved.</h2></div>');
+			res.write('<div style="display: flex; justify-content: center">');
+			res.write('<input type="button" value="List Registrations" onclick="javascript:window.location = \'/list\'"></div></html>');
 		} else {
-			res.write('<!DOCTYPE html><html style="position: relative; min-height: 100%; background: local url(data:image/gif;base64,'+bgdata+');">');
-			res.write('<head><meta charset="utf-8" /><meta name="viewport" /><title>Data Maintenance</title></head><body>\n');
+			res.write(headerbg.replace('~bg~',bgdata2)+'<head><meta charset="utf-8" /><meta name="viewport" />');
+			res.write('<title>Data Maintenance</title></head><body>\n');
 			res.write('<div style="display: flex; justify-content: center"><h1 style="color: #101010;">EDIT CRN :'+crn+'</h1></div>\n');
 			res.write('<div style="display: flex; justify-content: center">\n');
 			res.write('<form action="" method="get"><textarea name="json" style="width: 600px;height: 400px;">\n');
 			res.write(JSON.stringify(registrations, null, 4));
-			res.write('</textarea><br/><input type="submit" value="Save">&nbsp;<input type="button" value="Cancel" onclick="javascript:window.location = \'/list\'"></form></div></body></html>');
+			res.write('</textarea><br/><input type="submit" value="Save">&nbsp;');
+			res.write('<input type="button" value="Cancel" onclick="javascript:window.location = \'/list\'"></form></div></body></html>');
 		}
 	}
 }
@@ -153,4 +147,3 @@ app.get('/addnoniom/:crn/', (request, response)=> {
 app.listen(port, () => {
   console.log('Running on port ' + port);
 });
-
