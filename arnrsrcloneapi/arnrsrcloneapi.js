9d28fd8132db138bcd9c288f6502e92c937aea08
Your Name <yourmail@mail.com>
2021-11-03 18:36:21 +0000

    ARARSRCloningAPI added homepage

--- arnrsrcloneapi/arnrsrcloneapi.js
+++ arnrsrcloneapi/arnrsrcloneapi.js
@@ -25,39 +25,47 @@ function saveJSON() {
 	    if (err) { throw err; }
 	});
 }
-function securityHeader(req){
-	var secHeader = req.headers['authorization'];
+function securityHeader(request){
+	var secHeader = request.headers['authorization'];
 	if (secHeader !== undefined && secHeader.substring(0,7) == "Bearer "){ return true; } else { return false; }
 }
 /*--------------------------ENDPOINT FUNCTIONS--------------------------------*/
-function listFunc(res) {
-	res.write(headerbg.replace('~bg~',bgdata)+'\
+function homeFunc(response) {
+	response.write(headerbg.replace('~bg~',bgdata2)+'\
+	<div style="display:flex;justify-content:center">\
+	<h2 style="color: #101010;">ARN RSR Cloning API Stub</h2></div>\
+	<div style="display:flex;justify-content:center">\
+	<input type="button" value="List Episodes" onclick="javascript:window.location = \'/list\'"></div></html>');
+
+}
+function listFunc(response) {
+	response.write(headerbg.replace('~bg~',bgdata)+'\
 			<div style="display:flex;justify-content:center">\
 			<table style="border-spacing:1px;"><tr><th style="color:#101010;padding:10px;">CRN</th>\
 			<th style="color:#101010;padding:10px;">EPISODES (Assessment Answers)</th></tr>');
 	for (var i=0; i < offdata.crns.length; i++){
 		var bgcol = ((i+1)%2 === 0) ? 'B0BDDCC0' : 'DCCFB0C0'; //Odd&Even Colour
-		res.write('\
+		response.write('\
 			<tr><td style="background-color:#'+bgcol+';padding:5px;vertical-align:top;"><b>'+offdata.crns[i].crn+'</b>\
 			<br/><input type="button" value="Edit" onclick="javascript:window.location = \'/edit/'+offdata.crns[i].crn+'\'">\
 			</td><td style="background-color: #'+bgcol+';padding:5px">\
 			<pre>'+JSON.stringify(offdata.crns[i].episodes, null, 4)+'</pre></td></tr>');
 	}
-	res.write('</table></div></html>');
+	response.write('</table></div></html>');
 }
-function displayFunc(res, crn) {
+function displayFunc(response, crn) {
 	var found = 0;
 	for (var i=0; i< offdata.crns.length; i++){
 		if (offdata.crns[i].crn == crn) {
 			found++;
-			res.write('{\"episodes\": ');
-			res.write(JSON.stringify(offdata.crns[i].episodes, null, 4));
-			res.write('}');
+			response.write('{\"episodes\": ');
+			response.write(JSON.stringify(offdata.crns[i].episodes, null, 4));
+			response.write('}');
 		}
 	}
-	if (found === 0) res.write(notfound);
+	if (found === 0) response.write(notfound);
 }
-function editFunc(req, res, crn) {
+function editFunc(request, response, crn) {
 	var found = 0;
 	var foundindex = 0;
 	var episodes = '';
@@ -69,19 +77,19 @@ function editFunc(req, res, crn) {
 		}
 	}
 	if (found === 0) {
-		res.write(notfound+',{crn:'+crn+'}');
+		response.write(notfound+',{crn:'+crn+'}');
 	} else {
-		var data = req.query.json; // $_GET["json"]
+		var data = request.query.json; // $_GET["json"]
 		if (data !== undefined) {
 			offdata.crns[foundindex].episodes = JSON.parse(data);
 			saveJSON();
-			res.write(headerbg.replace('~bg~',bgdata2)+'\
+			response.write(headerbg.replace('~bg~',bgdata2)+'\
 				<div style="display:flex;justify-content:center">\
 				<h2 style="color: #101010;">Changes saved.</h2></div>\
 				<div style="display:flex;justify-content:center">\
-				<input type="button" value="List episodes" onclick="javascript:window.location = \'/list\'"></div></html>');
+				<input type="button" value="List Episodes" onclick="javascript:window.location = \'/list\'"></div></html>');
 		} else {
-			res.write(headerbg.replace('~bg~',bgdata2)+'\
+			response.write(headerbg.replace('~bg~',bgdata2)+'\
 				<head><meta charset="utf-8" /><meta name="viewport" /><title>Data Maintenance</title></head><body>\n\
 				<div style="display: flex; justify-content: center"><h1 style="color: #101010;">EDIT CRN :'+crn+'</h1></div>\n\
 				<div style="display: flex; justify-content: center">\n\
@@ -91,7 +99,7 @@ function editFunc(req, res, crn) {
 		}
 	}
 }
-function addFunc(res, crn) {
+function addFunc(response, crn) {
 	var found = 0;
 	for (var i=0; i< offdata.crns.length; i++){
 		if (offdata.crns[i].crn == crn) {
@@ -103,22 +111,24 @@ function addFunc(res, crn) {
 		newdata.crn = crn;
 		offdata.crns.push(newdata);
 		saveJSON();
-		listFunc(res);
+		listFunc(response);
 	} else {
-		res.write(headerbg.replace('~bg~',bgdata2)+'\
+		response.write(headerbg.replace('~bg~',bgdata2)+'\
 		<div style="display:flex;justify-content:center">\
 		<h2 style="color: #101010;">CRN Already Exists!!!</h2></div>\
 		<div style="display:flex;justify-content:center">\
-		<input type="button" value="List episodes" onclick="javascript:window.location = \'/list\'"></div></html>');
+		<input type="button" value="List Episodes" onclick="javascript:window.location = \'/list\'"></div></html>');
 	}
 }
 /*-----------------------------APP INITIALISE---------------------------------*/
-//	GET /subject/X259951/assessments/episodes/RSR/latest
-//  "crn: eg "X259951" – offender.cms_prob_number
-
 var offdata = loadJSON(jsonfile);
 const app = express();
 app.use(express.json());
+app.get('/', (request, response)=> {
+	response.writeHead(200, {'Content-Type': 'text/html'});
+	homeFunc(response);
+	response.end('');
+});
 app.get('/list', (request, response)=> {
 	response.writeHead(200, {'Content-Type': 'text/html'});
 	listFunc(response);
