f5f9534718531c3d179efa955af98ce6004337ca
Your Name <yourmail@mail.com>
2021-11-04 10:33:01 +0000

    ARNRSRCLONINGAPI - Changed the output and added delete button

--- arnrsrcloneapi/arnrsrcloneapi.js
+++ arnrsrcloneapi/arnrsrcloneapi.js
@@ -41,15 +41,16 @@ function homeFunc(response) {
 function listFunc(response) {
 	response.write(headerbg.replace('~bg~',bgdata)+'\
 			<div style="display:flex;justify-content:center">\
-			<table style="border-spacing:1px;"><tr><th style="color:#101010;padding:10px;">CRN</th>\
+			<table style="border-spacing:1px;"><tr><th style="color:#101010;padding:10px;"></th>\
 			<th style="color:#101010;padding:10px;">EPISODES (Assessment Answers)</th></tr>');
 	for (var i=0; i < offdata.crns.length; i++){
 		var bgcol = ((i+1)%2 === 0) ? 'B0BDDCC0' : 'DCCFB0C0'; //Odd&Even Colour
 		response.write('\
-			<tr><td style="background-color:#'+bgcol+';padding:5px;vertical-align:top;"><b>'+offdata.crns[i].crn+'</b>\
-			<br/><input type="button" value="Edit" onclick="javascript:window.location = \'/edit/'+offdata.crns[i].crn+'\'">\
+			<tr><td style="background-color:#'+bgcol+';padding:5px;vertical-align:top;"><b>CRN:'+offdata.crns[i].crn+'</b>\
+			<br/><br/><input type="button" value="Edit" onclick="javascript:window.location = \'/edit/'+offdata.crns[i].crn+'\'">\
+			<br/><br/><input type="button" value="Delete!" onclick="javascript:if(confirm(\'Are you sure you want to delete CRN '+offdata.crns[i].crn+'?\')) window.location = \'/delete/'+offdata.crns[i].crn+'\'">\
 			</td><td style="background-color: #'+bgcol+';padding:5px">\
-			<pre>'+JSON.stringify(offdata.crns[i].episodes, null, 4)+'</pre></td></tr>');
+			<textarea readonly style="min-height: 200px; min-width: 600px;background-color:#'+bgcol+';">'+JSON.stringify(offdata.crns[i].episodes, null, 4)+'</textarea></td></tr>');
 	}
 	response.write('</table></div></html>');
 }
@@ -120,6 +121,25 @@ function addFunc(response, crn) {
 		<input type="button" value="List Episodes" onclick="javascript:window.location = \'/list\'"></div></html>');
 	}
 }
+function deleteFunc(response, crn) {
+	var found = -1;
+	for (var i=0; i< offdata.crns.length; i++){
+		if (offdata.crns[i].crn == crn) {
+			found=i;
+		}
+	}
+	if (found > -1) {
+		offdata.crns.splice(found,1);
+		saveJSON();
+		listFunc(response);
+	} else {
+		response.write(headerbg.replace('~bg~',bgdata2)+'\
+		<div style="display:flex;justify-content:center">\
+		<h2 style="color: #101010;">CRN NOT Found!!!</h2></div>\
+		<div style="display:flex;justify-content:center">\
+		<input type="button" value="List Episodes" onclick="javascript:window.location = \'/list\'"></div></html>');
+	}
+}
 /*-----------------------------APP INITIALISE---------------------------------*/
 var offdata = loadJSON(jsonfile);
 const app = express();
@@ -154,6 +174,12 @@ app.get('/add/:crn/', (request, response)=> {
 	var crn = request.params.crn;
 	response.writeHead(200, {'Content-Type': 'text/html'});
 	addFunc(response,crn);
+	response.end('');
+});
+app.get('/delete/:crn/', (request, response)=> {
+	var crn = request.params.crn;
+	response.writeHead(200, {'Content-Type': 'text/html'});
+	deleteFunc(response,crn);
 	response.end('');
 });
 /*----------------------------START HTTP SERVER-------------------------------*/
